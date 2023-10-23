646c0e0b2c63af15c34c78891e8aa81ba1e92213
Anton <razjivin.anton@yandex.ru>
2023-10-23 21:55:22 +0300

    Сделал сортировку

--- main_bw.java
+++ main_bw.java
@@ -1,9 +1,11 @@
 package Java.Java_Seminar_6;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 
 /**
@@ -11,33 +13,34 @@ import java.util.Scanner;
  */
 public class main_bw {
     private static HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
-    private static LinkedHashMap<String, ArrayList<String>> linkPhoneBook = new LinkedHashMap<>();
+    //private static LinkedHashMap<String, ArrayList<String>> linkPhoneBook = new LinkedHashMap<>();
     public static void main(String[] args) {
-        // String a1 = "8563244";        
-        // String a2 = "8562314";        
-        // String a3 = "8652314";
-        // String a4 = "8546321";
-        // String a5 = "8965423";        
-        // String a6 = "8965132";
-        // String a7 = "8956475";
-        // String a8 = "8914782";
-        // String a9 = "8956482";
-        // String a10 = "895474";
+        String a1 = "8563244";        
+        String a2 = "8562314";        
+        String a3 = "8652314";
+        String a4 = "8546321";
+        String a5 = "8965423";        
+        String a6 = "8965132";
+        String a7 = "8956475";
+        String a8 = "8914782";
+        String a9 = "8956482";
+        String a10 = "895474";
 
-        // String s1 = "Anton";
-        // String s2 = "Stepan";
-        // String s3 = "Vova";
-        // String s4 = "Artem";
-        // add(s1, a1);
-        // add(s2, a2);
-        // add(s3, a3);
-        // add(s4, a4);        
-        // add(s1, a5);
-        // add(s1, a6);
-        // add(s2, a7);
-        // add(s3, a8);
-        // add(s3, a9);
-        // add(s1, a10);
+        String s1 = "Anton";
+        String s2 = "Stepan";
+        String s3 = "Vova";
+        String s4 = "Artem";
+        //phoneBook = new HashMap<>();
+        add(s1, a1);
+        add(s2, a2);
+        add(s3, a3);
+        add(s4, a4);        
+        add(s1, a5);
+        add(s1, a6);
+        add(s2, a7);
+        add(s3, a8);
+        add(s3, a9);
+        add(s1, a10);
 
         interfacePhoneBooke();
 
@@ -81,8 +84,7 @@ public class main_bw {
                 case 4:
                 System.out.print("Ввeдите имя: ");
                 String nameRem = iScanner.next();
-                phoneBook.remove(nameRem);
-                linkPhoneBook.remove(nameRem);
+                removeContact(nameRem);
                 break;
                 case 5: s = false;
                 default:
@@ -95,7 +97,9 @@ public class main_bw {
     }
 
     public static void printGetPhoneBook() {
-        sortphoneBook();
+        //sortphoneBook();
+        LinkedHashMap<String, ArrayList<String>> linkPhoneBook = sortphoneBook();
+        System.out.println("***************");
         for(var item: linkPhoneBook.entrySet()) {
             ArrayList<String> numb = new ArrayList<>();
             numb.addAll(item.getValue());
@@ -107,10 +111,13 @@ public class main_bw {
             for(String phone: numb) {
                 System.out.println(phone);
             }
+            
         }
+        System.out.println("***************");
     }
 
     public static void printFind(String name) {
+        System.out.println("***************");
         ArrayList<String> numb = new ArrayList<>();
         numb.addAll(find(name));
         if (phoneBook.get(name).size() > 1) {
@@ -121,6 +128,7 @@ public class main_bw {
         for(String item: numb) {
             System.out.println(item);
         }
+        System.out.println("***************");
     }
 
     public static void add(String s, String n) {
@@ -143,28 +151,32 @@ public class main_bw {
     }
 
 
-    public static void sortphoneBook() {
+    public static LinkedHashMap<String, ArrayList<String>> sortphoneBook() {
+        LinkedHashMap<String, ArrayList<String>> linkPhoneBook = new LinkedHashMap<>();
         ArrayList<String> str = new ArrayList<>();
         String strPhone = "";
         for(var item: phoneBook.entrySet()) {
             str.add(item.getKey());
         }
         for (int i = 0; i < str.size(); i++) {
-             for (var item: phoneBook.entrySet()) {
+            for (var item: phoneBook.entrySet()) {
+                //System.out.println(phoneBook.get(str.get(i)));
                 if(phoneBook.get(str.get(i)).size() > item.getValue().size()) {
                     strPhone = str.get(i);
-                } 
+                }
             }
-           linkPhoneBook.put(strPhone, phoneBook.get(strPhone));
+            if(strPhone != "") {
+                linkPhoneBook.put(strPhone, phoneBook.get(strPhone));
+            }
         }
         for(var item: phoneBook.entrySet()) {
             linkPhoneBook.putIfAbsent(item.getKey(), item.getValue());
         }
-        
+        return linkPhoneBook;
     }
 
-    public static void removeContact(String name) {
+    public static HashMap<String, ArrayList<String>> removeContact(String name) {
         phoneBook.remove(name);
-        linkPhoneBook.remove(name);
+        return phoneBook;
     }
 }
